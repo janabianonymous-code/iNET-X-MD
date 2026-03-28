a845943224d8d9d370a64b55cb5dc6c26eec2797
janabianonymous-code <janabianonymous@gmail.com>
2026-03-28 10:21:40 +0300

    Add configuration settings in config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  BOT_NAME: "iNET X-MD V2",
+  OWNER_NAME: "Configurable",
+  OWNER_NUMBER: "254700000000",
+  PREFIX: ".",
+  SESSION: "session",
+
+  AUTO_READ: true,
+  ANTI_LINK: true,
+  ANTI_DELETE: true,
+  ANTI_SPAM: true,
+
+  VERSION: "2.0.0"
+};
